526c7e5302a2cfbb39a9fb5a36b2e941c61b254b
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-05-26 12:03:52 +0700

    export and inport multiple component

---
+++ src/components/welcome/Welcome.jsx
@@ -0,0 +1,11 @@
+export default function WellcomeH1() {
+    return (
+        <h1>Welcome From H1</h1>
+    )
+}
+
+export function WelcomeH2() {
+    return (
+        <h2>Welcome From H2</h2>
+    )
+}
